e06b1fd2d1930534a74ac086296e08cca3b65d70
kimjisub <0226daniel@gmail.com>
2023-11-30 11:27:14 +0900

    feat: gui

--- src/Client/Main.java
+++ src/Client/Main.java
@@ -13,6 +13,7 @@ import java.awt.event.KeyEvent;
 import javax.swing.border.LineBorder;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
+import java.io.UnsupportedEncodingException;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
@@ -30,8 +31,16 @@ public class Main extends JFrame {
 	public static void createConnectFrame() {
 
 		try {
-			UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
-		}  catch (Exception e) { }
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+				if ("Nimbus".equals(info.getName())) {
+					UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+		} catch (Exception e) {
+			// Nimbus LookAndFeel을 설정하는데 실패하면 기본 시스템 LookAndFeel을 사용합니다.
+		}
 
 		connectFrame = new JFrame("채팅 프로그램 - 접속");
 		connectFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -61,8 +70,7 @@ public class Main extends JFrame {
 		});
 		hostField.setLocation(60,75);
 		hostField.setSize(270,25);
-		//hostField.setFont(new Font("Serif", Font.BOLD, 16));
-		hostField.setBorder(new LineBorder(Color.GRAY, 3));
+		hostField.setBorder(new LineBorder(new Color(0x45aaf2), 1));
 		hostField.setHorizontalAlignment(JTextField.CENTER);
 		hostField.setToolTipText("주소를 입력하세요.");
 
@@ -85,16 +93,14 @@ public class Main extends JFrame {
 		});
 		portField.setLocation(380,75);
 		portField.setSize(180,25);
-		portField.setBorder(new LineBorder(Color.GRAY, 3));
-		//portField.setFont(new Font("Serif", Font.BOLD, 16));
+		portField.setBorder(new LineBorder(new Color(0x45aaf2), 1));
 		portField.setHorizontalAlignment(JTextField.CENTER);
 		portField.setToolTipText("포트번호를 입력하세요.");
 
 		JTextField nameField = new JTextField(20);
 		nameField.setLocation(60,110);
 		nameField.setSize(150,30);
-		nameField.setBorder(new LineBorder(Color.GRAY, 3));
-		//nameField.setFont(new Font("Serif", Font.BOLD, 16));
+		nameField.setBorder(new LineBorder(new Color(0x45aaf2), 1));
 		nameField.setHorizontalAlignment(JTextField.CENTER);
 		nameField.setToolTipText("이름을 입력하세요.");
 
@@ -102,19 +108,19 @@ public class Main extends JFrame {
 		connectFrame.add(hos);
 		hos.setLocation(15,70);
 		hos.setSize(50,30);
-		hos.setFont(new Font("Serif", Font.BOLD, 16));
+		hos.setFont(new Font("Dialog", Font.BOLD, 16));
 
 		JLabel por = new JLabel("Port : ");
 		connectFrame.add(por);
 		por.setLocation(340,70);
 		por.setSize(50,30);
-		por.setFont(new Font("Serif", Font.BOLD, 16));
+		por.setFont(new Font("Dialog", Font.BOLD, 16));
 
 		JLabel nam = new JLabel("Name : ");
 		connectFrame.add(nam);
 		nam.setLocation(8,105);
 		nam.setSize(60,40);
-		nam.setFont(new Font("Serif", Font.BOLD, 16));
+		nam.setFont(new Font("Dialog", Font.BOLD, 16));
 
 
 		connectFrame.add(hostField);
@@ -126,9 +132,9 @@ public class Main extends JFrame {
 		connectFrame.add(connectButton);
 		connectButton.setLocation(380,110);
 		connectButton.setSize(70,40);
-		connectButton.setFont(new Font("Serif", Font.BOLD, 14)); // Serif 폰트, 굵은 스타일, 16pt 크기
-		connectButton.setForeground(Color.black); // 텍스트 색상 변경
-		connectButton.setBorder(new LineBorder(Color.lightGray, 2)); // 경계 스타일 변경
+		connectButton.setFont(new Font("Serif", Font.BOLD, 14));
+		connectButton.setForeground(new Color(0x45aaf2));
+		connectButton.setBorder(new LineBorder(new Color(0xd1d8e0), 1));
 
 		connectButton.setEnabled(false);
 		nameField.getDocument().addDocumentListener(new DocumentListener(){
@@ -156,7 +162,6 @@ public class Main extends JFrame {
 				if (portText.isEmpty()) {
 					port = 8080;
 				} else {
-					// 포트 필드에 값이 있으면 정수로 변환
 					port = Integer.parseInt(portText);
 				}
 
@@ -169,7 +174,7 @@ public class Main extends JFrame {
 							@Override
 							public void onMessageNew(int messageId, int userId, String message) {
 								try {
-									doc.insertString(doc.getLength(), message + "\n", leftAlign);
+									doc.insertString(doc.getLength(), userId + ": " + message + "\n", leftAlign);
 								} catch (BadLocationException e) {
 									e.printStackTrace();
 								}
@@ -214,8 +219,7 @@ public class Main extends JFrame {
 		});
 
 		connectFrame.setSize(600, 220);
-		//Color lightGreen = new Color(210, 255, 210);
-		//connectFrame.getContentPane().setBackground(lightGreen);
+		connectFrame.getContentPane().setBackground(new Color(0xdfe6e9));
 		connectFrame.setVisible(true);
 		connectFrame.setLocationRelativeTo(null);
 	}
@@ -260,10 +264,16 @@ public class Main extends JFrame {
 	private static void sendMessage(JTextField messageField) {
 		String message = messageField.getText();
 		if (!message.isEmpty() && client != null) {
-			client.sendMessage(message);
+			try {
+				byte[] messageBytes = message.getBytes("UTF-8");
+				client.sendMessage(new String(messageBytes, "UTF-8"));
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace(); // 적절한 예외 처리를 해야 합니다.
+			}
 			messageField.setText("");
 		}
 	}
 
 	public static void main(String[] args) {createConnectFrame();}
 }
+
